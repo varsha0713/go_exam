ff1d3b93d87799ec94200d5ab37b9656f589e536
Varsha A <99706376+varsha0713@users.noreply.github.com>
2023-09-11 22:36:24 +0000

    commit

---
+++ web.go
@@ -0,0 +1,17 @@
+package main
+import(
+	"fmt"
+	"log"
+	"net/http"
+)
+func main(){
+	var port string
+	fmt.Print("enter the port no to listen on:")
+	fmt.Scan(&port)
+
+	http.HandleFunc("/",func(w http.ResponseWriter,r *http.Request){
+		fmt.Fprintf(w,"Hello web")
+})
+log.Println("port is listening on",port,"......")
+http.ListenAndServe(":"+port,nil)
+}
